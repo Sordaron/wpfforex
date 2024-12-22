e8339e39c5feadabbda32600fc5c47b8e8f84ac1
SERHEO <SERHEO@DESKTOP-UUOB7GF>
2024-12-22 21:55:37 +0700

    Добавьте файлы проекта.

---
+++ wpfforex/EditCena.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace wpfforex
+{
+    /// <summary>
+    /// Логика взаимодействия для EditCena.xaml
+    /// </summary>
+    public partial class EditCena : Window
+    {
+        Products _cur;
+        public EditCena(Products _pr)
+        {
+            
+            InitializeComponent();
+            
+            if (_pr != null )
+            {
+                _cur = _pr;
+            }
+            DataContext = _cur;
+        }
+
+        private void MATYA_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ExamenEntities.GetContext().SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+            }
+            Close();
+        }
+    }
+}
---
+++ wpfforex/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpfforex
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            
+            CMB.SelectedValuePath = "Id";
+            CMB.DisplayMemberPath = "Name";
+            CMB.ItemsSource = ExamenEntities.GetContext().Categories.ToList();
+
+            DG.ItemsSource = ExamenEntities.GetContext().Products.ToList();
+        }
+
+        private void CMB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int IND = Convert.ToInt32(CMB.SelectedValue);
+            DG.ItemsSource = ExamenEntities.GetContext().Products.Where(x => x.Categorie == IND).ToList();
+        }
+
+        private void ButFil_Click(object sender, RoutedEventArgs e)
+        {
+            double a = Convert.ToDouble(one.Text);
+            double b = Convert.ToDouble(two.Text);
+            DG.ItemsSource = ExamenEntities.GetContext().Products.Where(x => x.Cost >= a && x.Cost < b).ToList();
+        }
+
+        private void Sort_Click(object sender, RoutedEventArgs e)
+        {
+            DG.ItemsSource = ExamenEntities.GetContext().Products.OrderByDescending(x => x.Cost).ToList();
+        }
+
+        private void Grusha_Click(object sender, RoutedEventArgs e)
+        {
+            EditCena editCena = new EditCena((sender as Button).DataContext as Products);
+            editCena.ShowDialog();
+            DG.ItemsSource = ExamenEntities.GetContext().Products.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Login.Text == "" || Password.Text == "")
+            {
+                MessageBox.Show("введите все поля");
+            }
+            else
+            {
+                Users a = ExamenEntities.GetContext().Users.FirstOrDefault(x => x.Login == Login.Text && x.Password == Password.Text);
+                if (a != null)
+                {
+                    a.Skidka = 5;
+                    ExamenEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Вы авторизованы," + a.Name);
+                }
+                
+            }
+        }
+
+        private void Grisha_Click(object sender, RoutedEventArgs e)
+        {
+            var g = (sender as Button).DataContext as Products;
+            if (g != null)
+            {
+                g.Count--;
+                ExamenEntities.GetContext().SaveChanges();
+                DG.ItemsSource = ExamenEntities.GetContext().Products.ToList();
+            }
+        }
+    }
+}
